3d41e9e2c96f467a4b3876c994711e6631316aad
Pedro Díaz Rivera <pedro.neox@gmail.com>
2023-06-05 21:21:17 -0400

    se crean clases Profesional y Accidente

---
+++ SprintJavaM4/src/model/Profesional.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Profesional extends Usuario{
+    private String titulo;
+    private String fechaIngreso;
+
+    public Profesional() {
+    }
+
+    public Profesional(String nombre, String fechaNacimiento, int run, String titulo, String fechaIngreso) {
+        super(nombre, fechaNacimiento, run);
+        this.titulo = titulo;
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(String fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    @Override
+    public String toString() {
+        return "Profesional{" +
+                "titulo='" + titulo + '\'' +
+                ", fechaIngreso='" + fechaIngreso + '\'' +
+                "} " + super.toString();
+    }
+}
